fd4034fa48a8c0d44e35635e3ea852c2964f5362
mogony <jelleam@opmbx.org>
2014-04-23 19:41:11 +0200

    Made the graph transparant

--- src/models/Graph.java
+++ src/models/Graph.java
@@ -5,18 +5,20 @@ import org.jfree.chart.*;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
- *
- * @author jelle
+ * Class that can be called to create a graph. 
+ * It does not draw it, call method getCp() and add it to a panel to show
+ * it on screen.
+ * @author Jelle Mogony, AMS04
  */
 public class Graph {
     DefaultCategoryDataset dcd;
     JFreeChart chart;
     ChartPanel cp;
 
-
     /**
-     * Creates a graph based on the parameter
-     * @param scores 
+     * Creates a graph based on the parameter 'scores'. 
+     * @param scores should include a patients name and a list of scores and 
+     * dates. 
      */
     public Graph(ArrayList<Scores> scores) {
         dcd = new DefaultCategoryDataset();
@@ -37,13 +39,10 @@ public class Graph {
         }
         
         chart = ChartFactory.createLineChart("Scores", "Date", "Score", dcd);
+        chart.setBackgroundPaint(new java.awt.Color(0xFF, 0xFF, 0xFF, 0)); //Transparant
         cp = new ChartPanel(chart);
     }
     
-    public void addValue() {
-        
-    }
-
     public DefaultCategoryDataset getDcd() {
         return dcd;
     }
