20f331726c9a851753d48ea62b3d99d1f1c3c7c9
mogony <jelleam@opmbx.org>
2014-05-05 15:55:08 +0200

    Created session file

---
+++ src/ldgraph/Session.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ldgraph;
+
+/**
+ *
+ * @author jelle
+ */
+public class Session {
+    public static int storedUserId;
+    public static String storedUserName;
+    public static String storedUserDob;
+}
